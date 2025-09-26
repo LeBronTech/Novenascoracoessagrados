343f2a54a999ed364d24d01ab44b8e176cb96815
Leandro José (Léo) <leandro2203jose@gmail.com>
2025-09-26 22:42:19 +0000

    favicon <link rel="icon" type="image/png" sizes="16x16"  href="/favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" sizes="16x16"  href="/favicons/favicon-16x16.png"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="theme-color" content="#ffffff"/>
       </head>
       <body className="font-body antialiased">
         {children}
