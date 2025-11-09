65f290639a0415f7d8695fadda431fc26a3cb67b
Leandro José (Léo) <leandro2203jose@gmail.com>
2025-11-09 04:58:52 +0000

    Ainda nada continua em cinza claro

--- src/components/weekly-devotions.tsx
+++ src/components/weekly-devotions.tsx
@@ -128,7 +128,7 @@ const WeeklyDevotions = () => {
           {/* Monthly Devotion */}
           <Tooltip>
             <TooltipTrigger asChild>
-              <div className='devotion-item group devotion-item--default'>
+              <div className='devotion-item group'>
                 <Calendar className="devotion-icon" />
                 <div className="text-left">
                     <span className="text-sm font-bold">{monthlyDevotion.name}</span>
